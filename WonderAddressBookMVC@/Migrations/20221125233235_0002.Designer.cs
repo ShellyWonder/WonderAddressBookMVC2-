b99fa7dc8f23ecec70c1c8068585ba50e5938858
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-11-25 17:38:07 -0600

    updated database

--- WonderAddressBookMVC@/Migrations/20221125233235_0002.Designer.cs
+++ WonderAddressBookMVC@/Migrations/20221125233235_0002.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WonderAddressBookMVC_.Data;
@@ -11,9 +12,11 @@ using WonderAddressBookMVC_.Data;
 namespace WonderAddressBookMVC.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221125233235_0002")]
+    partial class _0002
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
