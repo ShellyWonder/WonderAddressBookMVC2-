4bacfa71a385203dc5265a6b8277bf1b08c19c6a
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-11-26 15:12:43 -0600

    created IImageService & ImageService

---
+++ WonderAddressBookMVC@/Services/ImageService.cs
@@ -0,0 +1,43 @@
+﻿using WonderAddressBookMVC_.Services.Interfaces;
+
+namespace WonderAddressBookMVC_.Services
+{
+    public class ImageService : IImageService
+    {
+        #region Globals
+        private readonly string[] suffixes = { "Bytes", "KB", "MB", "GB", "TB", "PB" };
+        #endregion
+        public string ConvertByteArrayToFile(byte[] fileData, string extension)
+        {
+            try
+            {
+                string imageBase64Data = Convert.ToBase64String(fileData);
+                return string.Format($"data:{extension}; base64,{imageBase64Data}");
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #region Convert file to Byte Array
+        public async Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
+        {
+            try
+            {
+                using MemoryStream memoryStream = new MemoryStream();
+                await file.CopyToAsync(memoryStream);
+                byte[] byteFile = memoryStream.ToArray();
+                return byteFile;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+    }
+}
