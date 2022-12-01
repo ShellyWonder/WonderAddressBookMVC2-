080c9dc5ee3cb81d7e231f8871cfc21456383dba
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-12-01 15:19:42 -0600

    created models for the email feature

---
+++ WonderAddressBookMVC@/Models/EmailData.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WonderAddressBookMVC_.Models
+{
+    public class EmailData
+    {
+        [Required]
+        public string EmailAddress { get; set; } = string.Empty;
+        [Required]
+        public string Subject { get; set; } = string.Empty;
+        [Required]
+        public string Body { get; set; } = string.Empty;
+
+        public int id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set;}
+        public string? GroupName { get; set; }
+
+        [NotMapped]
+        public string? FullName { get { return $"{FirstName} {LastName}"; } }
+    }
+}
---
+++ WonderAddressBookMVC@/Models/ViewModels/EmailContactViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WonderAddressBookMVC_.Models.ViewModels
+{
+    public class EmailContactViewModel
+    {
+        //holds contact + the data coming from the Email Data class == multiple data sets
+        public Contact? Contact { get; set; }
+        public EmailData? EmailData { get; set; }
+    }
+}
