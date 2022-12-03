b7ca0ef70379538d1fdcdac47cdaf4c1224e96db
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-12-03 07:18:39 -0600

    created MailSettings model to enable email functionality

---
+++ WonderAddressBookMVC@/Models/MailSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace WonderAddressBookMVC_.Models
+{
+    public class MailSettings
+    {
+        //mimicks object properties in Mail Settings in user secrets
+        public string? Email { get; set; }
+        public string? Password  { get; set; }
+        public string? DisplayName { get; set; }
+        public string? Host { get; set; }
+        public int Port { get; set; }
+
+    }
+}
