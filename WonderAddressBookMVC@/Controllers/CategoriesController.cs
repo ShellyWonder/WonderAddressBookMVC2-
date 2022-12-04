9afada84164e9f400ebbada699488d2013f2581c
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-12-04 09:18:26 -0600

    Deleted unnecesary Categories Detail view and action

--- WonderAddressBookMVC@/Controllers/CategoriesController.cs
+++ WonderAddressBookMVC@/Controllers/CategoriesController.cs
@@ -16,47 +16,32 @@ namespace WonderAddressBookMVC_.Controllers
     {
         private readonly ApplicationDbContext _context;
 
+        #region Constructor
         public CategoriesController(ApplicationDbContext context)
         {
             _context = context;
         }
+        #endregion
 
+        #region Get Categories
         // GET: Categories
         public async Task<IActionResult> Index()
         {
             var applicationDbContext = _context.Categories.Include(c => c.AppUser);
             return View(await applicationDbContext.ToListAsync());
         }
+        #endregion
 
-        // GET: Categories/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .Include(c => c.AppUser)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
+        #region Get Categories Create
         // GET: Categories/Create
         public IActionResult Create()
         {
             ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id");
             return View();
         }
+        #endregion
 
-        // POST: Categories/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        #region POST: Categories/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,AppUserId,Name")] Category category)
@@ -70,8 +55,9 @@ namespace WonderAddressBookMVC_.Controllers
             ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", category.AppUserId);
             return View(category);
         }
+        #endregion
 
-        // GET: Categories/Edit/5
+        #region Get Categories Edit
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.Categories == null)
@@ -87,10 +73,9 @@ namespace WonderAddressBookMVC_.Controllers
             ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", category.AppUserId);
             return View(category);
         }
+        #endregion
 
-        // POST: Categories/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        #region POST: Categories Edit
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,AppUserId,Name")] Category category)
@@ -123,7 +108,9 @@ namespace WonderAddressBookMVC_.Controllers
             ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", category.AppUserId);
             return View(category);
         }
+        #endregion
 
+        #region Get Categories Delete
         // GET: Categories/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
@@ -142,7 +129,9 @@ namespace WonderAddressBookMVC_.Controllers
 
             return View(category);
         }
+        #endregion
 
+        #region POST Categories Delete
         // POST: Categories/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
@@ -157,14 +146,17 @@ namespace WonderAddressBookMVC_.Controllers
             {
                 _context.Categories.Remove(category);
             }
-            
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+        #endregion
 
+        #region Category Exists
         private bool CategoryExists(int id)
         {
-          return _context.Categories.Any(e => e.Id == id);
+            return _context.Categories.Any(e => e.Id == id);
         }
+        #endregion
     }
 }
